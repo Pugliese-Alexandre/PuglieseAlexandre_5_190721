ef6b8a59c3e0cb2226d7ab566c393af5476f6a22
Pugliese <Pugliese-Alexandre@live.be>
2021-08-29 11:52:46 +0200

    Bouton "Ajouter au panier" modifié
    Changement de couleur du bouton ajouter au panier

--- FrontEnd/js/produit.js
+++ FrontEnd/js/produit.js
@@ -17,6 +17,7 @@ fetch("http://localhost:3000/api/cameras/" + leId).then(response => response.jso
     let container = document.querySelector(".main__Produit_PageSecondaire");
 
     container.innerHTML = `
+
     <div class="produit_PageSecondaire">
     <div class="produit__Picture_PageSecondaire">
         <img class="image__Camera_PageSecondaire" src="${data.imageUrl}">
@@ -27,22 +28,46 @@ fetch("http://localhost:3000/api/cameras/" + leId).then(response => response.jso
             <li>Nom : <span class="produit__Name_PageSecondaire">${data.name}</span></li>
             <li>Description : <span class="produit__Description_PageSecondaire">${data.description}</span></li>
             <li>Prix : <span class="produit__Price_PageSecondaire">${displayPrice(data.price)}</span></li>
-    </br>  
+         </ul> 
             <form >
                 <label for="option__Produit">
                     <select name="option__Produit" id="option__Produit">
                     ${data.lenses.map(lense => '<option>' + lense +'</option>').join('')}
+                    </select>
                 </label>
-            </form>
-        </ul>
+        </form>
     </div>
-    
-<div class="ajouter__Panier">
-<button id="btn-envoyer" type="submit" name="btn-envoyer">Ajouter au panier</button>
-</div>
-
-    </div>
+       </div>
+<div class="bouton__Commander">
+       <button id="btn-envoyer" type="submit" name="btn-envoyer"> <i class="fas fa-shopping-cart" id="fas-btn"></i>Ajouter au panier</button>    
+       </div>
     `;
+    const idForm = document.querySelector("#option__Produit")
+    console.log(idForm)
+    //Sélection du bouton ajouter l'article au panier
+    const btn_envoyerPanier = document.querySelector("#btn-envoyer")
+    console.log("btn -", btn_envoyerPanier)
+
+    // Ecouter le bouton et envoyer le panier
+
+    btn_envoyerPanier.addEventListener("click", (event) => {
+        event.preventDefault()
+
+        //Mettre le choix de l'utilisateuir dans une variable 
+        const choixForm = idForm.value;
+
+        // Récupération des valeurs du formulaire 
+
+        let option__Produit = {
+            nomProduit: data.name,
+            idProduitSelectionner: data._id,
+            option__Produit: choixForm,
+            prix: data.price / 100
+        }
+
+        console.log(option__Produit);
+    });
+
 
 }).catch(error => {
     console.error("rien ici")
@@ -54,6 +79,19 @@ fetch("http://localhost:3000/api/cameras/" + leId).then(response => response.jso
 
 // Prix cents > euro 
 
-function displayPrice(price){
-    return(price / 100).toFixed(2) + "€"
+function displayPrice(price) {
+    return (price / 100).toFixed(2) + "€"
 }
+
+// Bouton Commander 
+
+var cart = 0
+var btnAdd = document.querySelector('#btn-envoyer')
+var element = document.querySelector('btn-envoyer')
+element.innerText = cart
+btnAdd.addEventListener('click', function(){
+
+    cart++
+    element.innerText = cart
+    console.log(cart);
+})
